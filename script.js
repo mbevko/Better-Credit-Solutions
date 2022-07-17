b7f163108ad879bfc7c6d6c36863c2860fd1aaab
mbevko <mbevko@gmail.com>
2022-07-17 03:43:32 -0400

    added modal and mobile navbar functionality, made nav links work and icons links

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 
-//Mobile Navbar functionality
+//Mobile Navbar and modal functionality
 let menuBtn = document.querySelector('.hamburger_menu');
 let mobNav = document.querySelector('.nav_mobile_closed');
+let modal = document.querySelector(".modal_closed");
+let navLinks = document.querySelectorAll(".u-list a")
 
 menuBtn.addEventListener('click', () => {
   mobNav.classList.toggle("nav_mobile")
-  
+  modal.classList.toggle("modal")
+})
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    mobNav.classList.toggle("nav_mobile") 
+    modal.classList.toggle("modal")
+  })
+})
+
+modal.addEventListener('click', () => {
+  mobNav.classList.toggle("nav_mobile")
+  modal.classList.toggle("modal")
 })
 
